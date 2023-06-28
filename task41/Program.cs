2d0548cdc82609797f61d3bc6b12e4273c30c3d2
Igor <test@mail.ru>
2023-06-28 15:52:03 +0300

    Добавил решение задачи 41 (task41)

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+//int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+//----------------- Создание функций, начало -------------------------------------------
+int[] ReadInt(string text)
+{
+   System.Console.Write(text);
+    int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    return array;
+}
+
+void SumF(int[] array, out int sum)
+{
+sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0) sum += array[i];
+}
+System.Console.WriteLine(sum);
+}
+//----------------------- Создание функций, конец ---------------------------------------
+int[] array = ReadInt("Введите числа через пробел. Клавиша 'Enter' - завершает ввод: ");
+SumF(array, out int sum);
